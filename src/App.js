26948e0b70a3a93b29b029e813a4f31e10350559
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-07 09:10:15 +0900

    Added flex-shrink-0

--- src/App.js
+++ src/App.js
@@ -340,7 +340,7 @@ const TaskItem = React.memo(({ syntax, checked, data }) => {
 			<Markdown className="hidden" syntax={syntax}>
 				{/* NOTE: Use md-blue-a200 because md-blue-a400 is
 				too dark and overwritten by attrs.strike.style */}
-				<Checkbox className="mr-2 w-4 h-4 text-md-blue-a200 shadow transition duration-150" style={checkboxStyle} {...$attrs} />
+				<Checkbox className="mr-2 flex-shrink-0 w-4 h-4 text-md-blue-a200 shadow transition duration-150" style={checkboxStyle} {...$attrs} />
 				<span>{toInnerReact(data)}</span>
 			</Markdown>
 		</Node>
