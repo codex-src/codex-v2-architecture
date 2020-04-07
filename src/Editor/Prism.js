ed6ce802b659ddfd04010903f103673e5d585a3a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-07 19:11:01 +0900

    Simplified Prism implementation

--- src/Editor/Prism.js
+++ src/Editor/Prism.js
@@ -1,3 +1,44 @@
 // Maps user-perceived language extensions to Prism
 // languages.
-export default {}
+const Prism = {}
+
+document.addEventListener("DOMContentLoaded", e => {
+	Object.assign(Prism, {
+		bash:       window.Prism.languages.bash,
+		c:          window.Prism.languages.c,
+		cpp:        window.Prism.languages.cpp,
+		css:        window.Prism.languages.css,
+		d:          window.Prism.languages.d,
+		diff:       window.Prism.languages.diff,
+		docker:     window.Prism.languages.docker,
+		dockerfile: window.Prism.languages.dockerfile,
+		git:        window.Prism.languages.git,
+		go:         window.Prism.languages.go,
+		graphql:    window.Prism.languages.graphql,
+		htm:        window.Prism.languages.html,
+		html:       window.Prism.languages.html,
+		http:       window.Prism.languages.http,
+		js:         window.Prism.languages.jsx,
+		json:       window.Prism.languages.json,
+		jsx:        window.Prism.languages.jsx, // Uses jsx
+		kotlin:     window.Prism.languages.kotlin,
+		php:        window.Prism.languages.php,
+		py:         window.Prism.languages.py,
+		rb:         window.Prism.languages.rb,
+		ruby:       window.Prism.languages.ruby,
+		rust:       window.Prism.languages.rust,
+		sass:       window.Prism.languages.sass,
+		sh:         window.Prism.languages["shell-session"],
+		sql:        window.Prism.languages.sql,
+		svg:        window.Prism.languages.svg,
+		swift:      window.Prism.languages.swift,
+		ts:         window.Prism.languages.tsx, // Uses tsx
+		tsx:        window.Prism.languages.tsx,
+		wasm:       window.Prism.languages.wasm,
+		xml:        window.Prism.languages.xml,
+		yaml:       window.Prism.languages.yaml,
+		yml:        window.Prism.languages.yml,
+	})
+})
+
+export default Prism
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./stylesheets/prism-custom.css"
 import "./stylesheets/tailwind-form-checkbox.css"
 import "./stylesheets/tailwind-codex-overrides.css"
 
-;(() => {
+// ;(() => {
 	// Render React DOM:
 	ReactDOM.render(
 		<React.StrictMode>
@@ -17,49 +17,49 @@ import "./stylesheets/tailwind-codex-overrides.css"
 		</React.StrictMode>,
 		document.getElementById("root"),
 	)
-	// Load PrismJS languages (asynchronous):
-	document.addEventListener("DOMContentLoaded", e => {
-		if (!window.Prism || !window.Prism.languages) {
-			// No-op
-			return
-		}
-		/* eslint-disable no-multi-spaces */
-		Prism.bash       = window.Prism.languages.bash
-		Prism.c          = window.Prism.languages.c
-		Prism.cpp        = window.Prism.languages.cpp
-		Prism.css        = window.Prism.languages.css
-		Prism.d          = window.Prism.languages.d
-		Prism.diff       = window.Prism.languages.diff
-		Prism.docker     = window.Prism.languages.docker
-		Prism.dockerfile = window.Prism.languages.dockerfile
-		Prism.git        = window.Prism.languages.git
-		Prism.go         = window.Prism.languages.go
-		// Prism.gql     = window.Prism.languages.graphql
-		Prism.graphql    = window.Prism.languages.graphql
-		// Prism.htm     = window.Prism.languages.html
-		Prism.html       = window.Prism.languages.html
-		Prism.http       = window.Prism.languages.http
-		Prism.js         = window.Prism.languages.jsx
-		Prism.json       = window.Prism.languages.json
-		Prism.jsx        = window.Prism.languages.jsx // Uses jsx
-		Prism.kotlin     = window.Prism.languages.kotlin
-		Prism.php        = window.Prism.languages.php
-		Prism.py         = window.Prism.languages.py
-		Prism.rb         = window.Prism.languages.rb
-		Prism.ruby       = window.Prism.languages.ruby
-		Prism.rust       = window.Prism.languages.rust
-		Prism.sass       = window.Prism.languages.sass
-		Prism.sh         = window.Prism.languages["shell-session"]
-		// Prism.shell   = window.Prism.languages["shell-session"]
-		Prism.sql        = window.Prism.languages.sql
-		Prism.svg        = window.Prism.languages.svg
-		Prism.swift      = window.Prism.languages.swift
-		Prism.ts         = window.Prism.languages.tsx // Uses tsx
-		Prism.tsx        = window.Prism.languages.tsx
-		Prism.wasm       = window.Prism.languages.wasm
-		Prism.xml        = window.Prism.languages.xml
-		Prism.yaml       = window.Prism.languages.yaml
-		Prism.yml        = window.Prism.languages.yml
-		/* eslint-enable no-multi-spaces */
-	})
-})()
+// 	// Load PrismJS languages (asynchronous):
+// 	document.addEventListener("DOMContentLoaded", e => {
+// 		if (!window.Prism || !window.Prism.languages) {
+// 			// No-op
+// 			return
+// 		}
+// 		/* eslint-disable no-multi-spaces */
+// 		Prism.bash       = window.Prism.languages.bash
+// 		Prism.c          = window.Prism.languages.c
+// 		Prism.cpp        = window.Prism.languages.cpp
+// 		Prism.css        = window.Prism.languages.css
+// 		Prism.d          = window.Prism.languages.d
+// 		Prism.diff       = window.Prism.languages.diff
+// 		Prism.docker     = window.Prism.languages.docker
+// 		Prism.dockerfile = window.Prism.languages.dockerfile
+// 		Prism.git        = window.Prism.languages.git
+// 		Prism.go         = window.Prism.languages.go
+// 		// Prism.gql     = window.Prism.languages.graphql
+// 		Prism.graphql    = window.Prism.languages.graphql
+// 		// Prism.htm     = window.Prism.languages.html
+// 		Prism.html       = window.Prism.languages.html
+// 		Prism.http       = window.Prism.languages.http
+// 		Prism.js         = window.Prism.languages.jsx
+// 		Prism.json       = window.Prism.languages.json
+// 		Prism.jsx        = window.Prism.languages.jsx // Uses jsx
+// 		Prism.kotlin     = window.Prism.languages.kotlin
+// 		Prism.php        = window.Prism.languages.php
+// 		Prism.py         = window.Prism.languages.py
+// 		Prism.rb         = window.Prism.languages.rb
+// 		Prism.ruby       = window.Prism.languages.ruby
+// 		Prism.rust       = window.Prism.languages.rust
+// 		Prism.sass       = window.Prism.languages.sass
+// 		Prism.sh         = window.Prism.languages["shell-session"]
+// 		// Prism.shell   = window.Prism.languages["shell-session"]
+// 		Prism.sql        = window.Prism.languages.sql
+// 		Prism.svg        = window.Prism.languages.svg
+// 		Prism.swift      = window.Prism.languages.swift
+// 		Prism.ts         = window.Prism.languages.tsx // Uses tsx
+// 		Prism.tsx        = window.Prism.languages.tsx
+// 		Prism.wasm       = window.Prism.languages.wasm
+// 		Prism.xml        = window.Prism.languages.xml
+// 		Prism.yaml       = window.Prism.languages.yaml
+// 		Prism.yml        = window.Prism.languages.yml
+// 		/* eslint-enable no-multi-spaces */
+// 	})
+// })()
