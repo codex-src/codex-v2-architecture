a93ad7b3afc488807d4a01420fb25835bc2ed48b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-09 13:53:32 +0900

    Added comments to HOC.js

--- src/Editor2/HOC.js
+++ src/Editor2/HOC.js
@@ -1,27 +1,32 @@
 import React from "react"
 
-// Higher order component for a paragraph component.
+// Higher order components that describe paragraph or
+// multiline paragraph components.
+
 export const Paragraph = ({ id, tag, style, ...props }) => (
-	React.createElement(
-		tag || "div",
-		{
-			id,
-			"style": { whiteSpace: "pre-wrap", ...style },
-			"data-paragraph": true,
-			...props,
+	React.createElement(tag || "div", {
+		id,
+		"style": {
+			// NOTE: white-space: pre-wrap is needed because of
+			// contenteditable
+			whiteSpace: "pre-wrap",
+			...style,
 		},
-	)
+		"data-paragraph": true,
+		...props,
+	})
 )
 
-// Higher order component for a multiline component.
 export const Multiline = ({ id, tag, style, ...props }) => (
-	React.createElement(
-		tag || "div",
-		{
-			id,
-			"style": { whiteSpace: "pre-wrap", ...style },
-			"data-multiline": true,
-			...props,
+	React.createElement(tag || "div", {
+		id,
+		"style": {
+			// NOTE: white-space: pre-wrap is needed because of
+			// contenteditable
+			whiteSpace: "pre-wrap",
+			...style,
 		},
-	)
+		"data-multiline": true,
+		...props,
+	})
 )
