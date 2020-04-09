4c8b12376185f7d68c6bff97a1ba799781ad4c61
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-09 13:23:14 +0900

    Added a context and ReactDOM render pattern

--- src/App2.js
+++ src/App2.js
@@ -1,11 +1,12 @@
 import Enum from "Enum"
 import React from "react"
+import ReactDOM from "react-dom"
 
 const Type = new Enum("P", // Paragraph
 )
 
 const Paragraph = ({ id, data }) => (
-	<div id={id} data-node>
+	<div id={id} data-block-element>
 		{data}
 	</div>
 )
@@ -14,14 +15,45 @@ const typeMap = {
 	[Type.P]: Paragraph,
 }
 
-const Editor = ({ state, setState }) => (
-	state.data.map(({ type: T, ...props }) => (
-		React.createElement(typeMap[T], {
-			key: props.id,
-			...props,
-		})
-	))
-)
+const EditorContext = React.createContext()
+
+const Editor = ({
+	id,       // Optional ID
+	tag,      // Optional tag (uses <div> by default)
+	state,    // Required
+	setState, // Required
+}) => {
+	const ref = React.useRef()
+
+	React.useEffect(() => {
+		const { Provider } = EditorContext
+		ReactDOM.render(
+			<Provider value={[state, setState]}>
+				{state.data.map(({ type: T, ...props }) => (
+					React.createElement(typeMap[T], {
+						key: props.id,
+						...props,
+					})
+				))}
+			</Provider>,
+			ref.current,
+		)
+	}, [state, setState])
+
+	return (
+		React.createElement(
+			tag || "div",
+			{
+				ref,
+
+				id,
+
+				contentEditable: true,
+				suppressContentEditableWarning: true,
+			},
+		)
+	)
+}
 
 function useEditor(initialValue, options = null) {
 	const [state, setState] = React.useState(() => ({
