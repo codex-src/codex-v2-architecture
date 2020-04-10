dfa993f9bde6eb650766d8b883c794887006a181
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-10 22:38:45 +0900

    Uncommented relevant code

--- src/Editor2/Editor.js
+++ src/Editor2/Editor.js
@@ -11,242 +11,6 @@ import uuidv4 from "uuid/v4"
 
 const DEBUG_MODE = true && process.env.NODE_ENV !== "production"
 
-// // Ascends to the nearest keyed-paragraph.
-// function ascendToID(editorRoot, node) {
-// 	// TODO: Scope to editorRoot
-// 	while (true) {
-// 		if (node.nodeType === Node.ELEMENT_NODE && node.id) {
-// 			// No-op
-// 			break
-// 		}
-// 		node = node.parentNode
-// 	}
-// 	return node
-// }
-
-// Extends the cursor IDs (up to two before and after).
-function extendPosIDs(data, [pos1, pos2]) {
-	let index1 = data.findIndex(each => each.id === pos1.id)
-	// let index2 = data.findIndex(each => each.id === pos2.id)
-	let index2 = index1
-	if (pos2.id !== pos1.id) {
-		index2 = data.findIndex(each => each.id === pos2.id)
-	}
-	// Guard bounds:
-	index1 -= 2
-	if (index1 < 0) {
-		index1 = 0
-	}
-	index2 += 2
-	if (index2 >= data.length) {
-		index2 = data.length - 1
-	}
-	return [data[index1].id, data[index2].id]
-}
-
-// Computes the DOM element root ID and offset from a range
-// data structure.
-function computeUUIDAndOffsetFromRange(editorRoot, range) {
-	// if (!range.node && !range.offset) {
-	// 	return { id: "", offset: "" }
-	// }
-
-	let { node } = range
-	while (node.nodeType !== Node.ELEMENT_NODE || !node.id) {
-		node = node.parentNode
-	}
-	const elementRoot = node
-	const { id } = elementRoot
-	// Recursively counts the offset from an element to a
-	// range node and offset.
-	let offset = 0
-	const recurse = element => {
-		for (const each of element.childNodes) {
-			if (each === range.node) {
-				offset += range.offset
-				// Stop recursion:
-				return true
-			}
-			offset += (each.nodeValue || "").length
-			if (recurse(each)) {
-				// Stop recursion:
-				return true
-			}
-			// NOTE: Use next.getAttribute instead of next.id
-			// because next.id always returns ""
-			const next = each.nextElementSibling
-			if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
-				offset++
-			}
-		}
-		return false
-	}
-	recurse(elementRoot)
-	return { id, offset }
-}
-
-// Computes a range data structure from a cursor data
-// structure.
-//
-// NOTE: Don’t mutate pos -- copy
-function computeRangeFromPos(editorRoot, { ...pos }) {
-	const elementRoot = document.getElementById(pos.id)
-	if (!elementRoot) {
-		throw new Error("computeRangeFromPos: no such uuid element")
-	}
-
-	let node = null
-	let offset = 0
-	const recurse = element => {
-		for (const each of element.childNodes) {
-			const { length } = each.nodeValue || ""
-			if (pos.offset - length <= 0) {
-				node = each
-				offset = pos.offset
-				// Stop recursion:
-				return true
-			}
-			pos.offset -= length
-			if (recurse(each)) {
-				// Stop recursion:
-				return true
-			}
-			// NOTE: Use next.getAttribute instead of next.id
-			// because next.id always returns ""
-			const next = each.nextElementSibling
-			if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
-				pos.offset--
-			}
-		}
-		return false
-	}
-	recurse(elementRoot)
-	return { node, offset }
-}
-
-// Compares two cursor data structures.
-function posAreSame(pos1, pos2) {
-	const ok = (
-		pos1.id === pos2.id &&
-		pos1.offset === pos2.offset
-	)
-	return ok
-}
-
-// Returns whether cursor data structures are empty.
-function posAreEmpty(pos1, pos2) {
-	return [pos1, pos2].every(each => !each.id && !each.offset)
-}
-
-// Synchronizes the DOM cursor to cursor data structures.
-//
-// TODO: Guard !selection.rangeCount?
-function syncPos(editorRoot, pos1, pos2) {
-	if (posAreEmpty(pos1, pos2)) {
-		// No-op
-		return
-	}
-	// Get the cursor data structures from the DOM cursors:
-	const selection = document.getSelection()
-	if (selection.rangeCount) {
-		const range = selection.getRangeAt(0)
-		const domPos1 = computeUUIDAndOffsetFromRange(editorRoot, { node: range.startContainer, offset: range.startOffset })
-		let domPos2 = { ...domPos1 }
-		if (!range.collapsed) {
-			domPos2 = computeUUIDAndOffsetFromRange(editorRoot, { node: range.endContainer, offset: range.endOffset })
-		}
-		// Compare the VDOM cursor data structures to the DOM data
-		// structures:
-		if (posAreSame(pos1, domPos1) && posAreSame(pos2, domPos2)) {
-			// No-op
-			return
-		}
-	}
-	// Synchronize the DOM cursor to the VDOM cursor data
-	// structures:
-	const range = document.createRange()
-	const r1 = computeRangeFromPos(editorRoot, pos1)
-	range.setStart(r1.node, r1.offset)
-	range.collapse()
-	let r2 = { ...r1 }
-	if (!posAreSame(pos1, pos2)) {
-		r2 = computeRangeFromPos(editorRoot, pos2)
-		range.setEnd(r2.node, r2.offset)
-	}
-	// // NOTE: syncTrees eagerly calls removeAllRanges
-	// if (selection.rangeCount) {
-	// 	selection.removeAllRanges()
-	// }
-	selection.addRange(range)
-}
-
-// Reads a DOM node.
-function readNode(node) {
-	return node.nodeValue || ""
-}
-
-// Recursively reads a DOM element.
-//
-// FIXME: DOM element root -> readElementRoot?
-function readElement(element) {
-	const recurse = readElement
-
-	let str = ""
-	for (const each of element.childNodes) {
-		if (each.nodeType === Node.TEXT_NODE) {
-			str += readNode(each)
-			continue
-		}
-		str += recurse(each)
-		const next = each.nextElementSibling
-		if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
-			str += "\n"
-		}
-	}
-	return str
-}
-
-// Reads an unparsed (raw) data structure from extended IDs.
-//
-// TODO: Remove editorRoot from parameters
-function readRawFromExtendedIDs(editorRoot, [startID, endID]) {
-	let startElement = document.getElementById(startID)
-	let endElement = startElement
-	if (endID !== startID) {
-		endElement = document.getElementById(endID)
-	}
-	// Re-extend the start element once:
-	//
-	// TODO: Conditionally re-extend? It’s confusing to always
-	// re-extend
-	if (startElement.previousElementSibling) {
-		startElement = startElement.previousElementSibling
-	}
-	const seenIDs = {}
-	const unparsed = []
-	while (startElement) {
-		let { id } = startElement
-		if (!id || seenIDs[id]) {
-			id = uuidv4()
-			startElement.id = id
-		}
-		seenIDs[id] = true
-		const raw = readElement(startElement)
-		// const range = str.split("\n").map((each, index) => ({
-		// 	id: !index ? id : uuidv4(),
-		// 	raw: each,
-		// }))
-		unparsed.push({ id, raw })
-		if (startElement === endElement) {
-			// No-op
-			break
-		}
-		startElement = startElement.nextElementSibling
-	}
-	// TODO: atStart, atEnd?
-	return unparsed
-}
-
 // Counts the offset from an element to a node.
 function countOffset(element, node) {
 	let offset = 0
@@ -260,9 +24,7 @@ function countOffset(element, node) {
 				return true
 			}
 			const next = each.nextElementSibling
-			if (next && next.getAttribute("data-node")) {
-				offset++
-			}
+			offset += next && Boolean(next.getAttribute("data-node"))
 		}
 		return false
 	}
@@ -303,18 +65,241 @@ function computePosFromRange(editorRoot, { ...range }) {
 	return pos
 }
 
-// Computes the cursor from a reference to a DOM node.
+// Computes the cursor data structures.
 function computePos(editorRoot) {
 	const range = document.getSelection().getRangeAt(0)
 	const rangeStart = { node: range.startContainer, offset: range.startOffset }
 	const pos1 = computePosFromRange(editorRoot, rangeStart)
 	let pos2 = { ...pos1 }
-	if (!range.collapsed) { // TODO: state.hasSelection?
+	if (!range.collapsed) {
 		const rangeEnd = { node: range.endContainer, offset: range.endOffset }
 		pos2 = computePosFromRange(editorRoot, rangeEnd)
 	}
 	return [pos1, pos2]
 }
+
+// // Extends the cursor IDs (up to two before and after).
+// function extendPosIDs(data, [pos1, pos2]) {
+// 	let index1 = data.findIndex(each => each.id === pos1.id)
+// 	// let index2 = data.findIndex(each => each.id === pos2.id)
+// 	let index2 = index1
+// 	if (pos2.id !== pos1.id) {
+// 		index2 = data.findIndex(each => each.id === pos2.id)
+// 	}
+// 	// Guard bounds:
+// 	index1 -= 2
+// 	if (index1 < 0) {
+// 		index1 = 0
+// 	}
+// 	index2 += 2
+// 	if (index2 >= data.length) {
+// 		index2 = data.length - 1
+// 	}
+// 	return [data[index1].id, data[index2].id]
+// }
+//
+// // Computes the DOM element root ID and offset from a range
+// // data structure.
+// function computeUUIDAndOffsetFromRange(editorRoot, range) {
+// 	// if (!range.node && !range.offset) {
+// 	// 	return { id: "", offset: "" }
+// 	// }
+//
+// 	let { node } = range
+// 	while (node.nodeType !== Node.ELEMENT_NODE || !node.id) {
+// 		node = node.parentNode
+// 	}
+// 	const elementRoot = node
+// 	const { id } = elementRoot
+// 	// Recursively counts the offset from an element to a
+// 	// range node and offset.
+// 	let offset = 0
+// 	const recurse = element => {
+// 		for (const each of element.childNodes) {
+// 			if (each === range.node) {
+// 				offset += range.offset
+// 				// Stop recursion:
+// 				return true
+// 			}
+// 			offset += (each.nodeValue || "").length
+// 			if (recurse(each)) {
+// 				// Stop recursion:
+// 				return true
+// 			}
+// 			// NOTE: Use next.getAttribute instead of next.id
+// 			// because next.id always returns ""
+// 			const next = each.nextElementSibling
+// 			if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
+// 				offset++
+// 			}
+// 		}
+// 		return false
+// 	}
+// 	recurse(elementRoot)
+// 	return { id, offset }
+// }
+//
+// // Computes a range data structure from a cursor data
+// // structure.
+// //
+// // NOTE: Don’t mutate pos -- copy
+// function computeRangeFromPos(editorRoot, { ...pos }) {
+// 	const elementRoot = document.getElementById(pos.id)
+// 	if (!elementRoot) {
+// 		throw new Error("computeRangeFromPos: no such uuid element")
+// 	}
+//
+// 	let node = null
+// 	let offset = 0
+// 	const recurse = element => {
+// 		for (const each of element.childNodes) {
+// 			const { length } = each.nodeValue || ""
+// 			if (pos.offset - length <= 0) {
+// 				node = each
+// 				offset = pos.offset
+// 				// Stop recursion:
+// 				return true
+// 			}
+// 			pos.offset -= length
+// 			if (recurse(each)) {
+// 				// Stop recursion:
+// 				return true
+// 			}
+// 			// NOTE: Use next.getAttribute instead of next.id
+// 			// because next.id always returns ""
+// 			const next = each.nextElementSibling
+// 			if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
+// 				pos.offset--
+// 			}
+// 		}
+// 		return false
+// 	}
+// 	recurse(elementRoot)
+// 	return { node, offset }
+// }
+//
+// // Compares two cursor data structures.
+// function posAreSame(pos1, pos2) {
+// 	const ok = (
+// 		pos1.id === pos2.id &&
+// 		pos1.offset === pos2.offset
+// 	)
+// 	return ok
+// }
+//
+// // Returns whether cursor data structures are empty.
+// function posAreEmpty(pos1, pos2) {
+// 	return [pos1, pos2].every(each => !each.id && !each.offset)
+// }
+//
+// // Synchronizes the DOM cursor to cursor data structures.
+// //
+// // TODO: Guard !selection.rangeCount?
+// function syncPos(editorRoot, pos1, pos2) {
+// 	if (posAreEmpty(pos1, pos2)) {
+// 		// No-op
+// 		return
+// 	}
+// 	// Get the cursor data structures from the DOM cursors:
+// 	const selection = document.getSelection()
+// 	if (selection.rangeCount) {
+// 		const range = selection.getRangeAt(0)
+// 		const domPos1 = computeUUIDAndOffsetFromRange(editorRoot, { node: range.startContainer, offset: range.startOffset })
+// 		let domPos2 = { ...domPos1 }
+// 		if (!range.collapsed) {
+// 			domPos2 = computeUUIDAndOffsetFromRange(editorRoot, { node: range.endContainer, offset: range.endOffset })
+// 		}
+// 		// Compare the VDOM cursor data structures to the DOM data
+// 		// structures:
+// 		if (posAreSame(pos1, domPos1) && posAreSame(pos2, domPos2)) {
+// 			// No-op
+// 			return
+// 		}
+// 	}
+// 	// Synchronize the DOM cursor to the VDOM cursor data
+// 	// structures:
+// 	const range = document.createRange()
+// 	const r1 = computeRangeFromPos(editorRoot, pos1)
+// 	range.setStart(r1.node, r1.offset)
+// 	range.collapse()
+// 	let r2 = { ...r1 }
+// 	if (!posAreSame(pos1, pos2)) {
+// 		r2 = computeRangeFromPos(editorRoot, pos2)
+// 		range.setEnd(r2.node, r2.offset)
+// 	}
+// 	// // NOTE: syncTrees eagerly calls removeAllRanges
+// 	// if (selection.rangeCount) {
+// 	// 	selection.removeAllRanges()
+// 	// }
+// 	selection.addRange(range)
+// }
+//
+// // Reads a DOM node.
+// function readNode(node) {
+// 	return node.nodeValue || ""
+// }
+//
+// // Recursively reads a DOM element.
+// //
+// // FIXME: DOM element root -> readElementRoot?
+// function readElement(element) {
+// 	const recurse = readElement
+//
+// 	let str = ""
+// 	for (const each of element.childNodes) {
+// 		if (each.nodeType === Node.TEXT_NODE) {
+// 			str += readNode(each)
+// 			continue
+// 		}
+// 		str += recurse(each)
+// 		const next = each.nextElementSibling
+// 		if (next && (next.getAttribute("data-node") || next.getAttribute("data-root"))) {
+// 			str += "\n"
+// 		}
+// 	}
+// 	return str
+// }
+//
+// // Reads an unparsed (raw) data structure from extended IDs.
+// //
+// // TODO: Remove editorRoot from parameters
+// function readRawFromExtendedIDs(editorRoot, [startID, endID]) {
+// 	let startElement = document.getElementById(startID)
+// 	let endElement = startElement
+// 	if (endID !== startID) {
+// 		endElement = document.getElementById(endID)
+// 	}
+// 	// Re-extend the start element once:
+// 	//
+// 	// TODO: Conditionally re-extend? It’s confusing to always
+// 	// re-extend
+// 	if (startElement.previousElementSibling) {
+// 		startElement = startElement.previousElementSibling
+// 	}
+// 	const seenIDs = {}
+// 	const unparsed = []
+// 	while (startElement) {
+// 		let { id } = startElement
+// 		if (!id || seenIDs[id]) {
+// 			id = uuidv4()
+// 			startElement.id = id
+// 		}
+// 		seenIDs[id] = true
+// 		const raw = readElement(startElement)
+// 		// const range = str.split("\n").map((each, index) => ({
+// 		// 	id: !index ? id : uuidv4(),
+// 		// 	raw: each,
+// 		// }))
+// 		unparsed.push({ id, raw })
+// 		if (startElement === endElement) {
+// 			// No-op
+// 			break
+// 		}
+// 		startElement = startElement.nextElementSibling
+// 	}
+// 	// TODO: atStart, atEnd?
+// 	return unparsed
+// }
 
 const Document = ({ data }) => (
 	data.map(({ type: T, ...props }) => (
@@ -388,38 +373,36 @@ const Editor = ({ id, tag, state, setState }) => {
 						onBlur:  () => setState(current => ({ ...current, focused: false })),
 
 						onSelect: () => {
-							// const selection = document.getSelection()
-							// if (!selection.rangeCount) {
-							// 	// No-op
-							// 	return
-							// }
-							// // Correct the range when the editor DOM
-							// // element is focused:
-							// const range = selection.getRangeAt(0)
-							// if (range.startContainer === editorRootRef.current || range.endContainer === editorRootRef.current) {
-							// 	// Iterate to the deepest start node:
-							// 	let startNode = editorRootRef.current.childNodes[0]
-							// 	while (startNode.childNodes.length) {
-							// 		startNode = startNode.childNodes[0]
-							// 	}
-							// 	// Iterate to the deepest end node:
-							// 	let endNode = editorRootRef.current.childNodes[editorRootRef.current.childNodes.length - 1]
-							// 	while (endNode.childNodes.length) {
-							// 		endNode = endNode.childNodes[endNode.childNodes.length - 1]
-							// 	}
-							// 	// Correct the range:
-							// 	range.setStart(startNode, 0)
-							// 	range.setEnd(endNode, (endNode.nodeValue || "").length)
-							// 	selection.removeAllRanges()
-							// 	selection.addRange(range)
-							// }
+							const selection = document.getSelection()
+							if (!selection.rangeCount) { // TODO: Needed?
+								// No-op
+								return
+							}
+							// Correct out of bounds range:
+							const range = selection.getRangeAt(0)
+							if (range.startContainer === editorRootRef.current || range.endContainer === editorRootRef.current) {
+								// Iterate to the deepest start node:
+								let startNode = editorRootRef.current.childNodes[0]
+								while (startNode.childNodes.length) {
+									startNode = startNode.childNodes[0]
+								}
+								// Iterate to the deepest end node:
+								let endNode = editorRootRef.current.childNodes[editorRootRef.current.childNodes.length - 1]
+								while (endNode.childNodes.length) {
+									endNode = endNode.childNodes[endNode.childNodes.length - 1]
+								}
+								// Correct range:
+								range.setStart(startNode, 0)
+								range.setEnd(endNode, (endNode.nodeValue || "").length)
+								selection.removeAllRanges()
+								selection.addRange(range)
+							}
 							const [pos1, pos2] = computePos(editorRootRef.current)
-							console.log(pos1)
-							// setState(current => ({
-							// 	...current,
-							// 	pos1,
-							// 	pos2,
-							// }))
+							setState(current => ({
+								...current,
+								pos1,
+								pos2,
+							}))
 							// // TODO: Rename to extendPos? Use state
 							// // instead of state.data? Should extendedPos
 							// // be a member of state?
@@ -430,17 +413,17 @@ const Editor = ({ id, tag, state, setState }) => {
 							// pointerDown.current = true
 						},
 						onPointerMove: () => {
-							// // Editor must be focused and pointer must be down:
-							// if (!state.focused || !pointerDown.current) {
-							// 	pointerDown.current = false // Reset to be safe
-							// 	return
-							// }
-							// const [pos1, pos2] = computePos(editorRootRef.current)
-							// setState(current => ({
-							// 	...current,
-							// 	pos1,
-							// 	pos2,
-							// }))
+							// Editor must be focused and pointer must be down:
+							if (!state.focused || !pointerDown.current) {
+								pointerDown.current = false // Reset to be safe
+								return
+							}
+							const [pos1, pos2] = computePos(editorRootRef.current)
+							setState(current => ({
+								...current,
+								pos1,
+								pos2,
+							}))
 							// // TODO: Rename to extendPos? Use state
 							// // instead of state.data? Should extendedPos
 							// // be a member of state?
