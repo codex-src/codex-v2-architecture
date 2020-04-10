276997c43dd8507bb7481e25c16c267bd5a4df65
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-10 16:16:04 +0900

    syncPos is now working (minimally tested -- no actual tests)

--- src/Editor2/Editor.js
+++ src/Editor2/Editor.js
@@ -89,7 +89,7 @@ function computeUUIDAndOffsetFromRange(rootElement, range) {
 //
 // NOTE: Don’t mutate pos -- copy
 function computeRangeFromPos(rootElement, { ...pos }) {
-	const uuidElement = document.getElementByID(pos.id)
+	const uuidElement = document.getElementById(pos.id)
 	if (!uuidElement) {
 		throw new Error("computeRangeFromPos: no such uuid element")
 	}
@@ -148,22 +148,23 @@ function syncPos(rootElement, pos1, pos2) {
 
 	// Get the cursor data structures from the DOM cursors:
 	const selection = document.getSelection()
-	// if (selection.rangeCount) {
-	const range = selection.getRangeAt(0)
-	const domPos1 = computeUUIDAndOffsetFromRange(rootElement, { node: range.startContainer, offset: range.startOffset })
-	let domPos2 = { ...domPos1 }
-	if (!range.collapsed) {
-		domPos2 = computeUUIDAndOffsetFromRange(rootElement, { node: range.endContainer, offset: range.endOffset })
+	if (selection.rangeCount) {
+		const range = selection.getRangeAt(0)
+		const domPos1 = computeUUIDAndOffsetFromRange(rootElement, { node: range.startContainer, offset: range.startOffset })
+		let domPos2 = { ...domPos1 }
+		if (!range.collapsed) {
+			domPos2 = computeUUIDAndOffsetFromRange(rootElement, { node: range.endContainer, offset: range.endOffset })
+		}
+		// Compare the VDOM cursor data structures to the DOM data
+		// structures:
+		if (posAreSame(pos1, domPos1) && posAreSame(pos2, domPos2)) {
+			// No-op
+			return
+		}
 	}
-	// Compare the VDOM cursor data structures to the DOM data
-	// structures:
-	if (posAreSame(pos1, domPos1) && posAreSame(pos2, domPos2)) {
-		// No-op
-		return
-	}
-	// }
 	// Synchronize the DOM cursor to the VDOM cursor data
 	// structures:
+	const range = document.createRange()
 	const r1 = computeRangeFromPos(rootElement, pos1)
 	range.setStart(r1.node, r1.offset)
 	range.collapse()
@@ -422,179 +423,181 @@ const Editor = ({ id, tag, state, setState }) => {
 	const { Provider } = EditorContext
 	return (
 		<Provider value={[state, setState]}>
+			<div>
 
-			{React.createElement(
-				tag || "div",
-				{
-					ref,
+				{React.createElement(
+					tag || "div",
+					{
+						ref,
 
-					id,
+						id,
 
-					style: {
-						// NOTE: Imperative styles needed because of
-						// contenteditable
-						whiteSpace: "pre-wrap",
-						outline: "none",
-						overflowWrap: "break-word",
-					},
-
-					onFocus: () => setState(current => ({ ...current, focused: true })),
-					onBlur:  () => setState(current => ({ ...current, focused: false })),
-
-					onSelect: () => {
-						const selection = document.getSelection()
-						if (!selection.rangeCount) {
-							// No-op
-							return
-						}
-						// Correct the selection when the editor is
-						// selected instead of the innermost start and
-						// end nodes (expected behavior):
-						const range = selection.getRangeAt(0)
-						if (range.startContainer === ref.current || range.endContainer === ref.current) {
-							// Iterate to the innermost start node:
-							let startNode = ref.current.childNodes[0]
-							while (startNode.childNodes.length) {
-								startNode = startNode.childNodes[0]
-							}
-							// Iterate to the innermost end node:
-							let endNode = ref.current.childNodes[ref.current.childNodes.length - 1]
-							while (endNode.childNodes.length) {
-								endNode = endNode.childNodes[endNode.childNodes.length - 1]
-							}
-							// Correct the selection:
-							const range = document.createRange()
-							range.setStart(startNode, 0)
-							range.setEnd(endNode, (endNode.nodeValue || "").length)
-							selection.removeAllRanges()
-							selection.addRange(range)
-						}
-						const { selected, pos1, pos2 } = computePos(ref.current)
-						setState(current => ({ ...current, selected, pos1, pos2 }))
-						extendedIDs.current = extendPosIDs([pos1, pos2], state.data)
-					},
-
-					onPointerDown: () => {
-						pointerDown.current = true
-					},
-					onPointerMove: () => {
-						// Editor must be focused and pointer must be down:
-						if (!state.focused || !pointerDown.current) {
-							pointerDown.current = false // Reset to be safe
-							return
-						}
-						const { selected, pos1, pos2 } = computePos(ref.current)
-						setState(current => ({ ...current, selected, pos1, pos2 }))
-						extendedIDs.current = extendPosIDs([pos1, pos2], state.data)
-					},
-					onPointerUp: () => {
-						pointerDown.current = false
-					},
-
-					onKeyDown: e => {
-						// // Tab:
-						// if (!e.ctrlKey && e.keyCode === KEY_CODE_TAB) {
-						// 	e.preventDefault()
-						// 	dispatch.tab()
-						// 	return
-						// // Enter:
-						// } else if (e.keyCode === KEY_CODE_ENTER) {
-						// 	e.preventDefault()
-						// 	dispatch.enter()
-						// 	return
-						// // Undo:
-						// } else if (detect.undo(e)) {
-						// 	e.preventDefault()
-						// 	dispatch.undo()
-						// 	return
-						// // Redo:
-						// } else if (detect.redo(e)) {
-						// 	e.preventDefault()
-						// 	dispatch.redo()
-						// 	return
-						// }
-
-						// Guard the control key (browser shorcut):
-						if (!e.ctrlKey && e.keyCode === KeyCode.Tab) {
-							e.preventDefault()
-							let fn = tab
-							// Use detab if the shift key was pressed and
-							// the selection is multiline:
-							if (e.shiftKey && state.pos1.id !== state.pos2.id) {
-								fn = detab
-							}
-							fn(state, setState)
-						}
-					},
-
-					onInput: () => {
-						const unparsed = readRawFromExtendedIDs(ref.current, extendedIDs.current)
-						const parsed = parse(unparsed)
-
-						const x1 = state.data.findIndex(each => each.id === unparsed[0].id)
-						if (x1 === -1) {
-							throw new Error("onInput: x1 is out of bounds")
-						}
-						const x2 = state.data.findIndex(each => each.id === unparsed.slice(-1)[0].id)
-						if (x2 === -1) {
-							throw new Error("onInput: x2 is out of bounds")
-						}
-
-						// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-						const data = [...state.data]
-						data.splice(x1, (x2 + 1) - x1, ...parsed)
-
-						setState(current => ({
-							...current,
-							focused: false,  // DEBUG
-							selected: false, // DEBUG
-							data,
-							// TODO: pos1 and pos2
-						}))
-
-						// state.body.splice(x1, (x2 + 1) - x1, ...nodes)
-
-						// console.log(x1, x2)
-
-						// const key1 = nodes[0].key
-						// const x1 = state.body.findIndex(each => each.key === key1)
-						// if (x1 === -1) {
-						// 	throw new Error("FIXME")
-						// }
-						// const key2 = nodes[nodes.length - 1].key
-						// const x2 = !atEnd ? state.body.findIndex(each => each.key === key2) : state.body.length - 1
-						// if (x2 === -1) {
-						// 	throw new Error("FIXME")
-						// }
-						// state.body.splice(x1, (x2 + 1) - x1, ...nodes)
-						// // Update data, pos1, and pos2:
-						// const data = state.body.map(each => each.data).join("\n")
-						// Object.assign(state, { data, pos1, pos2 })
-						// this.render()
-
-						// const { nodes, atEnd } = getNodesFromIterators(ref.current, target.current)
-						// const [pos1, pos2] = getPos(ref.current)
-						// dispatch.actionInput(nodes, atEnd, pos1, pos2)
-					},
-
-					contentEditable: !state.readOnly, // Inverse
-					suppressContentEditableWarning: !state.readOnly, // Inverse
-				},
-			)}
-
-			{DEBUG_MODE && (
-				<div className="py-6 whitespace-pre-wrap font-mono text-xs leading-snug" style={{ tabSize: 2 }}>
-					{JSON.stringify(
-						{
-							...state,
-							reactDOM: undefined, // Obscure
+						style: {
+							// NOTE: Imperative styles needed because of
+							// contenteditable
+							whiteSpace: "pre-wrap",
+							outline: "none",
+							overflowWrap: "break-word",
 						},
-						null,
-						"\t",
-					)}
-				</div>
-			)}
 
+						onFocus: () => setState(current => ({ ...current, focused: true })),
+						onBlur:  () => setState(current => ({ ...current, focused: false })),
+
+						onSelect: () => {
+							const selection = document.getSelection()
+							if (!selection.rangeCount) {
+								// No-op
+								return
+							}
+							// Correct the selection when the editor is
+							// selected instead of the innermost start and
+							// end nodes (expected behavior):
+							const range = selection.getRangeAt(0)
+							if (range.startContainer === ref.current || range.endContainer === ref.current) {
+								// Iterate to the innermost start node:
+								let startNode = ref.current.childNodes[0]
+								while (startNode.childNodes.length) {
+									startNode = startNode.childNodes[0]
+								}
+								// Iterate to the innermost end node:
+								let endNode = ref.current.childNodes[ref.current.childNodes.length - 1]
+								while (endNode.childNodes.length) {
+									endNode = endNode.childNodes[endNode.childNodes.length - 1]
+								}
+								// Correct the selection:
+								const range = document.createRange()
+								range.setStart(startNode, 0)
+								range.setEnd(endNode, (endNode.nodeValue || "").length)
+								selection.removeAllRanges()
+								selection.addRange(range)
+							}
+							const { selected, pos1, pos2 } = computePos(ref.current)
+							setState(current => ({ ...current, selected, pos1, pos2 }))
+							extendedIDs.current = extendPosIDs([pos1, pos2], state.data)
+						},
+
+						onPointerDown: () => {
+							pointerDown.current = true
+						},
+						onPointerMove: () => {
+							// Editor must be focused and pointer must be down:
+							if (!state.focused || !pointerDown.current) {
+								pointerDown.current = false // Reset to be safe
+								return
+							}
+							const { selected, pos1, pos2 } = computePos(ref.current)
+							setState(current => ({ ...current, selected, pos1, pos2 }))
+							extendedIDs.current = extendPosIDs([pos1, pos2], state.data)
+						},
+						onPointerUp: () => {
+							pointerDown.current = false
+						},
+
+						onKeyDown: e => {
+							// // Tab:
+							// if (!e.ctrlKey && e.keyCode === KEY_CODE_TAB) {
+							// 	e.preventDefault()
+							// 	dispatch.tab()
+							// 	return
+							// // Enter:
+							// } else if (e.keyCode === KEY_CODE_ENTER) {
+							// 	e.preventDefault()
+							// 	dispatch.enter()
+							// 	return
+							// // Undo:
+							// } else if (detect.undo(e)) {
+							// 	e.preventDefault()
+							// 	dispatch.undo()
+							// 	return
+							// // Redo:
+							// } else if (detect.redo(e)) {
+							// 	e.preventDefault()
+							// 	dispatch.redo()
+							// 	return
+							// }
+
+							// Guard the control key (browser shorcut):
+							if (!e.ctrlKey && e.keyCode === KeyCode.Tab) {
+								e.preventDefault()
+								let fn = tab
+								// Use detab if the shift key was pressed and
+								// the selection is multiline:
+								if (e.shiftKey && state.pos1.id !== state.pos2.id) {
+									fn = detab
+								}
+								fn(state, setState)
+							}
+						},
+
+						onInput: () => {
+							const unparsed = readRawFromExtendedIDs(ref.current, extendedIDs.current)
+							const parsed = parse(unparsed)
+
+							const x1 = state.data.findIndex(each => each.id === unparsed[0].id)
+							if (x1 === -1) {
+								throw new Error("onInput: x1 is out of bounds")
+							}
+							const x2 = state.data.findIndex(each => each.id === unparsed.slice(-1)[0].id)
+							if (x2 === -1) {
+								throw new Error("onInput: x2 is out of bounds")
+							}
+
+							// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+							const data = [...state.data]
+							data.splice(x1, (x2 + 1) - x1, ...parsed)
+
+							setState(current => ({
+								...current,
+								focused: false,  // DEBUG
+								selected: false, // DEBUG
+								data,
+								// TODO: pos1 and pos2
+							}))
+
+							// state.body.splice(x1, (x2 + 1) - x1, ...nodes)
+
+							// console.log(x1, x2)
+
+							// const key1 = nodes[0].key
+							// const x1 = state.body.findIndex(each => each.key === key1)
+							// if (x1 === -1) {
+							// 	throw new Error("FIXME")
+							// }
+							// const key2 = nodes[nodes.length - 1].key
+							// const x2 = !atEnd ? state.body.findIndex(each => each.key === key2) : state.body.length - 1
+							// if (x2 === -1) {
+							// 	throw new Error("FIXME")
+							// }
+							// state.body.splice(x1, (x2 + 1) - x1, ...nodes)
+							// // Update data, pos1, and pos2:
+							// const data = state.body.map(each => each.data).join("\n")
+							// Object.assign(state, { data, pos1, pos2 })
+							// this.render()
+
+							// const { nodes, atEnd } = getNodesFromIterators(ref.current, target.current)
+							// const [pos1, pos2] = getPos(ref.current)
+							// dispatch.actionInput(nodes, atEnd, pos1, pos2)
+						},
+
+						contentEditable: !state.readOnly, // Inverse
+						suppressContentEditableWarning: !state.readOnly, // Inverse
+					},
+				)}
+
+				{DEBUG_MODE && (
+					<div className="py-6 whitespace-pre-wrap font-mono text-xs leading-snug" style={{ tabSize: 2 }}>
+						{JSON.stringify(
+							{
+								...state,
+								reactDOM: undefined, // Obscure
+							},
+							null,
+							"\t",
+						)}
+					</div>
+				)}
+
+			</div>
 		</Provider>
 	)
 }
