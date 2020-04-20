bce2626fa57fb5a1ace441524425efddbcf2ca5d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-20 18:29:35 +0900

    Added back localStorage capabilities. Found a bug related to backspace at the start of a blockquote, e.g.:
    <cursor>> Hello, world!
    
    When a user backspaces there, an out of bounds error is emitted. This will most likely be fixed with custom backspace event handling.

--- src/App2.js
+++ src/App2.js
@@ -10,30 +10,6 @@ import useEditorSettings from "EditorSettings/useEditorSettings"
 
 import "./App.css"
 
-// ;(() => {
-// 	document.body.classList.toggle("debug-css")
-// })()
-
-// const LOCALSTORAGE_KEY = "codex-app-v2.3"
-//
-// const data = (() => {
-// 	const cache = localStorage.getItem(LOCALSTORAGE_KEY)
-// 	if (!cache) {
-// 		return raw("./App.md")
-// 	}
-// 	const json = JSON.parse(cache)
-// 	if (!json.data) {
-// 		return raw("./App.md")
-// 	}
-// 	return json.data
-// })()
-
-const data = `I seriously agree with this article. I find TypeScript to be a kind of exchange of productivity for correctness, which, to be honest, in the real-world is less practical. Think about it this way — would you rather have end-to-end tests that test your production-ready application or TypeScript complain in development? Think carefully, because the answer is not obvious. **TypeScript takes time to get right. A lot of time. This is time you could be testing your application _in the wild_, rather than testing its correctness in development.** Yes, there _is_ some crossover here, but it’s not 100%. When you use TypeScript, you are **betting on TypeScript** helping you more than hurting you.
-
-What I’m trying to say is that TypeScript is a _very steep bet_. But something about this is unsettling — Go is not dynamic, and I find writing Go to be easier than TypeScript, so what gives? I actually think the crux of the problem is that TypeScript is trying to fix JavaScript. _But what if JavaScript doesn’t need fixing?_ Then TypeScript actually doesn’t pay for itself. I say all of this as a cautionary tale for developers. I’ve been turned on and off multiple times by TypeScript. Ultimately, I think that some languages introduce so much complexity up front that if you try to wrangle them in later, you’re optimizing for the wrong problem.
-
-I’m sure some of you will say that TypeScript makes you more productive, and if you are one of these people, that’s great. But I found I ran into similar problems as Lucas — TypeScript’s documentation and error messages are far from friendly, and TypeScript as a system starts to break down the more complexity you introduce into your app, e.g. recursive types, etc. I’m having bugs where my IDE and app don’t even agree. And I simply don’t have the time to find the root cause of every problem I run into, because most of these problems are concerned with correctness.`
-
 const ReadmeEditor = () => {
 	const [state, dispatch] = useEditor(raw("./Readme.md"))
 	return <Editor style={{ fontSize: 15 }} state={state} dispatch={dispatch} />
@@ -128,23 +104,32 @@ const FixedEditorSettings = ({ state, dispatch }) => (
 const keyCodeEsc = 27
 const keyCodeP = 80
 
+const typeScriptRant = `I seriously agree with this article. I find TypeScript to be a kind of exchange of productivity for correctness, which, to be honest, in the real-world is less practical. Think about it this way — would you rather have end-to-end tests that test your production-ready application or TypeScript complain in development? Think carefully, because the answer is not obvious. **TypeScript takes time to get right. A lot of time. This is time you could be testing your application _in the wild_, rather than testing its correctness in development.** Yes, there _is_ some crossover here, but it’s not 100%. When you use TypeScript, you are **betting on TypeScript** helping you more than hurting you.
+
+What I’m trying to say is that TypeScript is a _very steep bet_. But something about this is unsettling — Go is not dynamic, and I find writing Go to be easier than TypeScript, so what gives? I actually think the crux of the problem is that TypeScript is trying to fix JavaScript. _But what if JavaScript doesn’t need fixing?_ Then TypeScript actually doesn’t pay for itself. I say all of this as a cautionary tale for developers. I’ve been turned on and off multiple times by TypeScript. Ultimately, I think that some languages introduce so much complexity up front that if you try to wrangle them in later, you’re optimizing for the wrong problem.
+
+I’m sure some of you will say that TypeScript makes you more productive, and if you are one of these people, that’s great. But I found I ran into similar problems as Lucas — TypeScript’s documentation and error messages are far from friendly, and TypeScript as a system starts to break down the more complexity you introduce into your app, e.g. recursive types, etc. I’m having bugs where my IDE and app don’t even agree. And I simply don’t have the time to find the root cause of every problem I run into, because most of these problems are concerned with correctness.`
+
+const LOCALSTORAGE_KEY = "codex-app-v2.3"
+
+const data = (() => {
+	const cache = localStorage.getItem(LOCALSTORAGE_KEY)
+	if (!cache) {
+		return typeScriptRant // raw("./App.md")
+	}
+	const json = JSON.parse(cache)
+	if (!json.data) {
+		return typeScriptRant // raw("./App.md")
+	}
+	return json.data
+})()
+
 const App = () => {
 	// TODO: Can we use props.children instead of useEditor?
 	const [editor, editorDispatch] = useEditor(data)
 	const [editorSettings, editorSettingsDispatch] = useEditorSettings(renderModesEnum.Readme)
 
-	// // Writes editor.data to localStorage.
-	// React.useEffect(() => {
-	// 	const id = setTimeout(() => {
-	// 		const json = JSON.stringify({ data: editor.data })
-	// 		localStorage.setItem(LOCALSTORAGE_KEY, json)
-	// 	}, 100)
-	// 	return () => {
-	// 		clearTimeout(id)
-	// 	}
-	// }, [editor.data])
-
-	// Debounces editorSettingsDispatch.update by one frame.
+	// Debounces renderers by one frame.
 	React.useEffect(() => {
 		if (!editorSettings.showSidebar) {
 			// No-op
@@ -157,6 +142,19 @@ const App = () => {
 			clearTimeout(id)
 		}
 	}, [editor, editorSettings, editorSettingsDispatch])
+
+
+	// Writes editor.data to localStorage (debounced 100ms).
+	React.useEffect(() => {
+		const id = setTimeout(() => {
+			const json = JSON.stringify({ data: editor.data })
+			localStorage.setItem(LOCALSTORAGE_KEY, json)
+		}, 100)
+		return () => {
+			clearTimeout(id)
+		}
+	// TODO: Can use [editor.reactVDOM] instead
+	}, [editor.data])
 
 	// Binds read-only shortcut (macOS).
 	//
