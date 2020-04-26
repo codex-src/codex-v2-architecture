d52ca6d138abb6608cd60027d8aeb14695b00448
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-04-26 20:48:53 +0900

    Tweaked package.json

--- src/App2.js
+++ src/App2.js
@@ -497,6 +497,7 @@ const App = () => {
 						leaveTo="opacity-0 transform scale-95"
 					>
 						<Button className="px-4 py-3 flex flex-row items-center font-medium text-white bg-black rounded-lg shadow-lg pointer-events-auto" onClick={editorPrefsDispatch.toggleReadOnly}>
+							{/* <div className="w-px" /> */}
 							<svg className="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
 								{/* <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path> */}
 								{/* <path d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" fillRule="evenodd"></path> */}
@@ -504,6 +505,7 @@ const App = () => {
 							</svg>
 							Preview mode
 							<span className="ml-4 text-gray-300">Unlock</span>
+							{/* <div className="w-px" /> */}
 						</Button>
 					</Transition>
 				</div>
