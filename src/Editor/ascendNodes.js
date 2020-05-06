cb8608c12147124f6f78f365d45847c367e75085
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-06 19:25:38 +0900

    Fixed ascendNodes implementation

--- src/Editor/ascendNodes.js
+++ src/Editor/ascendNodes.js
@@ -9,30 +9,19 @@ function ascendToElement(node) {
 
 // Ascends to the nearest data-codex-node or data-codex-root
 // element.
-//
-// TODO: Add bounds check e.g. add editorRoot as a parameter
 export function ascendNode(node) {
-	// FIXME
-	node = ascendToElement(node)
-	while (node && node.parentElement && (
-		!node.getAttribute || (
-			!node.getAttribute("data-codex-node") &&
-			!node.getAttribute("data-codex-root")
-		)
-	)) {
-		node = node.parentElement
+	let element = ascendToElement(node)
+	while (element && element.parentElement && !(element.getAttribute("data-codex-node") || element.getAttribute("data-codex-root"))) {
+		element = element.parentElement
 	}
-	return node
+	return element
 }
 
 // Ascends to the nearest data-codex-root element.
-//
-// TODO: Add bounds check e.g. add editorRoot as a parameter
 export function ascendRoot(node) {
-	// FIXME
-	node = ascendToElement(node)
-	while (node && node.parentElement && (!node.getAttribute || !node.getAttribute("data-codex-root"))) {
-		node = node.parentElement
+	let element = ascendToElement(node)
+	while (element && element.parentElement && !element.getAttribute("data-codex-root")) {
+		element = element.parentElement
 	}
-	return node
+	return element
 }
