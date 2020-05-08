90d4297ad545d8aec5eeb0258b9251030e763d95
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-09 01:47:49 +0900

    Updated checkboxes to use onpointerdown exclusively

--- src/Editor/decorator.js
+++ src/Editor/decorator.js
@@ -1,24 +1,28 @@
 // Returns a new decorator function for the return of
 // deeplySyncNodes.
 function decorator(state, dispatch) {
-	const decorate = syncedNodes => {
-		for (const each of syncedNodes) {
-			if (each.nodeType !== Node.ELEMENT_NODE || (each.nodeName !== "UL" && each.nodeName !== "OL")) {
+	const decorate = syncedElements => {
+		for (const each of syncedElements) {
+			if (each.nodeName !== "UL" && each.nodeName !== "OL") {
 				// No-op
 				continue
 			}
-			const todo = each.querySelectorAll("[data-codex-node] > .absolute > [data-codex-checkbox]")
-			for (const each of todo) {
+			const checkboxes = each.querySelectorAll("[data-codex-node] > .absolute > [data-codex-checkbox]")
+			for (const each of checkboxes) {
 				const { id } = each.parentElement.parentElement
 				each.onpointerdown = e => {
 					e.preventDefault()
-				}
-				each.onclick = () => {
 					// Blur to prevent auto-scrolling:
 					document.activeElement.blur()
 					dispatch.checkTodo(id)
 					each.focus()
 				}
+				// each.onclick = () => {
+				// 	// Blur to prevent auto-scrolling:
+				// 	document.activeElement.blur()
+				// 	dispatch.checkTodo(id)
+				// 	each.focus()
+				// }
 			}
 		}
 	}
