9fcd9fce0e02dcc6eaf6e4000cfa2066a5575c66
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-19 01:04:33 +0900

    Extracted newURLHashEpoch

--- src/Editor/parser/parseElements.js
+++ src/Editor/parser/parseElements.js
@@ -1,3 +1,4 @@
+import newURLHashEpoch from "./newURLHashEpoch"
 import parseAnyList from "./parseAnyList"
 import parseInlineElements from "./parseInlineElements"
 import typeEnum from "../Elements/typeEnum"
@@ -19,32 +20,6 @@ import {
 	testPreformattedEnd,
 	testPreformattedStart,
 } from "./testers"
-
-// Creates a new URL hash epoch.
-function newURLHashEpoch() {
-	const hashes = {}
-	const newURLHash = str => {
-		// ALPHA / DIGIT / "-" / "." / "_" / "~"
-		//
-		// https://tools.ietf.org/html/rfc3986
-		//
-		/* eslint-disable no-useless-escape */
-		const hash = str
-			.toLowerCase()               // Lowercase
-			.replace(/(\s+|\-+)/g, "-")  // Convert spaces to dashes
-			.replace(/[^a-z0-9\-]/g, "") // Remove non-alphanumerics (strict)
-			.replace(/\-+/g, "-")        // Remove extraneous dashes (1 of 2)
-			.replace(/(^\-|\-$)/g, "")   // Remove extraneous dashes (2 of 2)
-		/* eslint-enable no-useless-escape */
-		const seen = hashes[hash]
-		if (!seen) {
-			hashes[hash] = 0
-		}
-		hashes[hash]++
-		return hash + (!seen ? "" : `-${hashes[hash]}`)
-	}
-	return newURLHash
-}
 
 function testFastPass(char) {
 	const ok = (
