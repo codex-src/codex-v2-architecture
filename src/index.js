69ff88965d91f96acd7ead951e09106efad50509
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-18 19:26:59 +0900

    Refactored CSS files -- added em-context pattern (not integrated into editor)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import ReactDOM from "react-dom"
 
 import "debug.css"
 
-import "stylesheets/prism_custom.css"
-import "stylesheets/tailwind.generated.css"
+import "stylesheets/material-design/color-vars.css"
+import "stylesheets/prism-js/custom.css"
+import "stylesheets/tailwind-css/color-vars.css"
+import "stylesheets/tailwind-css/em-context.css"
+import "stylesheets/tailwind-css/tailwind.generated.css"
 
 ReactDOM.render(
 	<App />,
