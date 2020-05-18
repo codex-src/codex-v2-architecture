ca67a9013922bf33d55213d1c3e3a5dc791f2a5d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-18 19:06:20 +0900

    Better comments for download

--- src/lib/download.js
+++ src/lib/download.js
@@ -1,6 +1,8 @@
 import React from "react"
 import renderDOM from "lib/renderDOM"
 
+// Downloads a file to the local filesystem. Parameters blob
+// and options passthrough to window.URL.createObjectURL.
 function download(filename, blob, options) {
 	const href = window.URL.createObjectURL(blob, options)
 	// eslint-disable-next-line jsx-a11y/anchor-has-content
