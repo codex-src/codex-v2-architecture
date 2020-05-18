a1da42cc2cbae2152b19bc8fe315e8b115589f0e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-18 19:13:03 +0900

    Better comments for Button

--- src/lib/Button.js
+++ src/lib/Button.js
@@ -1,5 +1,8 @@
 import React from "react"
 
+// Renders a button that does **not** focus when pressed.
+//
+// https://github.com/WICG/focus-visible
 const Button = props => (
 	<button onPointerDown={e => e.preventDefault()} {...props} />
 )
